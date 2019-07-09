90d182e9d52b0b73a427ec2dfee15fc7ae0c0dff
Matt Bell <matthewaustinbell@gmail.com>
2019-07-08 21:31:20 -0500

    single

--- src/components/SingleScat/SingleScat.js
+++ src/components/SingleScat/SingleScat.js
@@ -1,12 +1,39 @@
 import React from 'react';
 
+import scatData from '../../helpers/data/scats';
+
 import './SingleScat.scss';
 
 class SingleScat extends React.Component {
+  state = {
+    scat: {},
+  }
+
+  componentDidMount() {
+    const scatId = this.props.match.params.id;
+    scatData.getSIngleScat(scatId)
+      .then(scatPromise => this.setState({ scat: scatPromise.data }))
+      .catch(err => console.error('unable to get single scat', err));
+  }
+
+  deleteScat = () => {
+    const scatId = this.props.match.params.id;
+    scatData.deleteScat(scatId)
+      .then(() => this.props.history.push('/home'))
+      .catch(err => console.error('could not get scats', err));
+  }
+
   render() {
+    const { scat } = this.state;
     return (
       <div className="SingleScat">
-        <h1>Single Scat</h1>
+        <h1>{scat.sampleName}</h1>
+        <h2>{scat.location}</h2>
+        <h3>{scat.animal}</h3>
+        <h5>{scat.color}</h5>
+        <h6>{scat.weight}</h6>
+        <button className="btn btn-danger" onClick={this.deleteMe}>Delete</button>
+
       </div>
     );
   }
