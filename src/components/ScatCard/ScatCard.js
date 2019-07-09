75932aba286f5d43fc831b64d841a008d2d2c8aa
Matt Bell <matthewaustinbell@gmail.com>
2019-07-08 20:39:32 -0500

    wip

--- src/components/ScatCard/ScatCard.js
+++ src/components/ScatCard/ScatCard.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import scatShape from '../../helpers/propz/scatShape';
 
 class ScatCard extends React.Component {
+  static propTypes = {
+    scat: scatShape.scatShape,
+  }
+
   render() {
     const { scat } = this.props;
     return (
       <div className="ScatCard col-4">
         <div class="card">
-  <div class="card-body">
-    <h5 class="card-title">{scat.sampleName}</h5>
-    <p class="card-text">{scat.location}</p>
-    {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+          <div class="card-body">
+          <h5 class="card-title">{scat.sampleName}</h5>
+          <p class="card-text">{scat.location}</p>
+          {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
           </div>
         </div>
       </div>
