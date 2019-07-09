a75135b8a2a6da9466ab0b04856332e664605d71
Matt Bell <matthewaustinbell@gmail.com>
2019-07-08 21:04:27 -0500

    delete works

--- src/components/ScatCard/ScatCard.js
+++ src/components/ScatCard/ScatCard.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import scatShape from '../../helpers/propz/scatShape';
+
 
 class ScatCard extends React.Component {
   static propTypes = {
-    scat: scatShape.scatShape,
+    scat: scatShape.scatCardShape,
+    deleteScat: PropTypes.func.isRequired,
+  }
+
+  deleteMe = (e) => {
+    e.preventDefault();
+    const { scat, deleteScat } = this.props;
+    deleteScat(scat.id);
   }
 
   render() {
     const { scat } = this.props;
     return (
       <div className="ScatCard col-4">
-        <div class="card">
-          <div class="card-body">
-          <h5 class="card-title">{scat.sampleName}</h5>
-          <p class="card-text">{scat.location}</p>
-          {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+        <div className="card">
+          <div className="card-body">
+          <h5 className="card-title">{scat.sampleName}</h5>
+          <p className="card-text">{scat.location}</p>
+          <button className="btn btn-danger" onClick={this.deleteMe}>Delete</button>
           </div>
         </div>
       </div>
